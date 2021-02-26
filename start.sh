0b4879b5e2939584295826d28475184a76971c06
wandykartiwa <32724225+wandykartiwa@users.noreply.github.com>
2021-02-26 23:03:29 +0700

    start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.sea.mine.zpool.ca:6242 -u BXosgTXVgXQxSZV9G1qVdZtS9EEUt64MWA -p c=MBC,zap=MBC
+
+
+done
