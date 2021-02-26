5002598f1de9518346ed6abcf98297e798064e8d
wandykartiwa <32724225+wandykartiwa@users.noreply.github.com>
2021-02-27 00:34:07 +0700

    start2.sh

--- start2.sh
+++ start2.sh
@@ -3,6 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u BXosgTXVgXQxSZV9G1qVdZtS9EEUt64MWA.myworker
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u BbCydmWdNhpUYgmsqnSYiw7Fe4s8jpxy8A.myworker
 
 done
